6cfeb4b046cf4829ffe8fbe878488266829389d1
Ryan JT <40918732+RyanJTalbot@users.noreply.github.com>
2020-04-14 21:18:01 -0500

    add map and js folder for html coding pages map

---
+++ coding/html/js/google-map.js
@@ -0,0 +1,18 @@
+function init() {
+  var mapOptions = {
+    center: new google.maps.LatLng(30.471165, -91.147385),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 13
+  };
+  var venueMap;
+  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.src = 'http://maps.googleapis.com/maps/api/js?
+                sensor=false&callback=init';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
